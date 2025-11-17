17fbe9f63661d1974f6f6950a185b99ca115ce13
Hamza Omer <hamza.omer.zaki@gmail.com>
2025-11-17 10:53:42 +0300

    [Fix] Resolve ImportError for padding in PKI verification

--- app/crypto/pki.py
+++ app/crypto/pki.py
@@ -3,6 +3,7 @@ from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding # <--- ADDED THIS
 
 CA_CERT_PATH = "certs/ca_cert.pem"
 
@@ -37,7 +38,7 @@ def verify_certificate(cert_pem: str, expected_cn: str = None) -> bool:
         root_ca.public_key().verify(
             cert.signature,
             cert.tbs_certificate_bytes,
-            x509.padding.PKCS1v15(),
+            padding.PKCS1v15(), # <--- FIXED: Uses the correct imported padding module
             cert.signature_hash_algorithm,
         )
 
